e0c6379ce1dd3874bda63604a9098de6b0935cd2
Gabriel Sadigursky <61071956+GSNCunha@users.noreply.github.com>
2022-03-25 09:26:34 -0400

    organize the index.js file

--- index.js
+++ index.js
@@ -1,32 +1,95 @@
-   function printResults(searchClass, page) {
-       console.log(document.querySelector('.searchBar').value);
-       fetch('img/desktop.json')
+let page = "landing";
+let selectedImage= "";
+printResults('.mainContainer',page)
+document.querySelector('.searchBar').addEventListener("keyup",  function(){
+   if(page == "landing")
+   {
+    printResults('.mainContainer',page, null)
+   }
+   else
+   {
+    document.querySelector('.products').innerHTML = ``;
+    showSelected('.mainContainer', selectedImage,page);
+   }
+
+});
+document.querySelector('.mainContainer').onclick = function(event) {
+    page = "second";
+    selectedImage = event.target.id;
+    document.querySelector('.products').innerHTML = ``;
+    showSelected('.mainContainer', event.target.id,page);
+}
+//////////////////////////////////////////////////////////////
+
+function printResults(searchClass,page, selected)
+{
+    let buffer1 = `
+        <div>
+            <main>
+        <div class="products">
+        `;
+    
+    let buffer2 = `
+        <div class="wrapper grid">
+        `; 
+
+    let buffer3 =`
+                </div>
+            </main>
+        </div>
+        `;
+
+    rendering(searchClass,buffer1,buffer2,buffer3,page, selected);
+}
+
+function showSelected(searchClass, selected, page)
+{
+    let buffer1 = `
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+            <nav>
+                <div class="navigationProducts">
+        `;
+    
+    let buffer2 = `
+        <div>
+        `; 
+
+    let buffer3 =`
+            </div>
+            </nav>
+        </div>
+            `;
+    rendering(searchClass,buffer1,buffer2,buffer3,page, selected);
+
+}
+
+
+//////////////////////////////////////////////////////////////
+function rendering(searchClass,buffer1,buffer2,buffer3,page, selected)
+{
+
+    console.log(document.querySelector('.searchBar').value);
+    fetch('img/desktop.json')
     .then(res => res.json())
     .then(data =>{
         const database = data;
         const search = document.querySelector('.searchBar').value;
-       // console.log(${database.children[i].children[y].path);
         clearResults();
         let counter = 0;
-
         let buffer = "";
-        buffer = `
-              <div>
-                <main>
-                <div class="products">
-        `;
+
+        buffer += buffer1;
+
         for(let i =0; i < database.children.length; i++){
 
             let temporaryBuffer= "";
             let haveItems = false;
-            temporaryBuffer += `
-                
-            <div class="category"> 
-                <h3>${database.children[i].name}</h3>
-            </div>
-            
-            <div class="wrapper grid">
-            `; 
+            temporaryBuffer +=`
+                <div class="category"> 
+                    <h3>${database.children[i].name}</h3>
+                </div>
+            `;
+            temporaryBuffer += buffer2; 
 
             for(let y = 0; y < database.children[i].children.length; y++){
                 if(database.children[i].children[y].name.toLocaleLowerCase().includes(search) || search == "") 
@@ -47,21 +110,17 @@
             `;   
             if(haveItems)
             {
- 
+
                 buffer += temporaryBuffer;
 
             }
         }
-        buffer +=`
-          </div>
-        </main>
-    </div>
-        `;
+        buffer += buffer3;
         document.querySelector(searchClass).innerHTML = buffer;
         document.querySelector('.counter').innerHTML = `${counter} layouts found`;
 
 
-       /* if(document.querySelector(searchClass).innerHTML == "")
+    /* if(document.querySelector(searchClass).innerHTML == "")
         {
             document.querySelector(searchClass).innerHTML = "No results...";
         }*/
@@ -71,146 +130,33 @@
             document.querySelector(searchClass). innerHTML = "";
         }
 
+        ////////////////rendering selected image//////////////////////////
+        if(page == "second")
+        {
+            for(let i =0; i < database.children.length; i++){
+                for(let y = 0; y < database.children[i].children.length; y++){
+                    if(database.children[i].children[y].name == selected) 
+                    {
+                        document.querySelector(searchClass).innerHTML += `
+                        
+                        <div class="col-xs-12 col-sm-9 col-md-6 col-lg-7">
+                        <main>
+                          <div class="products">
+                                <img id='${database.children[i].children[y].name}' class="selectedImage" src='../img/${database.children[i].children[y].path}' alt='${database.children[i].children[y].name}'>
+                          </div>
+                        </main>
+                    </div>
+    
+                        `;
+                        break;
+                    }
+                }
+            }
+        }
+
     })
     .catch((error)=>{
         console.error(error);
     })
 }
-////////////////first render/////////////////
-////////////////////////////////////////////
-/////////////////////////////////////////////
-
-
-    function showSelected(searchClass, selected, page)
-    {
-        fetch('img/desktop.json')
-        .then(res => res.json())
-        .then(data =>{
-            const database = data;
-            const search = document.querySelector('.searchBar').value;
-           // console.log(${database.children[i].children[y].path);
-            clearResults();
-            let counter = 0;
-    
-            let buffer = "";
-            buffer = `
-                <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
-                    <nav>
-                        <div class="navigationProducts">
-            `;
-            for(let i =0; i < database.children.length; i++){
-    
-                let temporaryBuffer= "";
-                let haveItems = false;
-                temporaryBuffer += `
-                    
-                <div class="category"> 
-                    <h3>${database.children[i].name}</h3>
-                </div>
-                
-                <div>
-                `; 
-    
-                for(let y = 0; y < database.children[i].children.length; y++){
-                    if(database.children[i].children[y].name.toLocaleLowerCase().includes(search) || search == "") 
-                    {
-                        counter++;
-                        haveItems = true;
-                        temporaryBuffer += `
-                        <div class="item">
-                            <figure class="fig">
-                                <img id='${database.children[i].children[y].name}' class="img" type="button" src='../img/${database.children[i].children[y].path}' alt='${database.children[i].children[y].name}'>
-                                <figcaption>${database.children[i].children[y].name}</figcaption>
-                            </figure>
-                        </div>
-                        `; 
-                    }
-                }
-                temporaryBuffer += `</div>
-                `;   
-                if(haveItems)
-                {
-     
-                    buffer += temporaryBuffer;
-    
-                }
-            }
-            buffer +=`
-            </div>
-            </nav>
-          </div>
-
-            `;
-            document.querySelector(searchClass).innerHTML = buffer;
-            document.querySelector('.counter').innerHTML = `${counter} layouts found`;
-    
-    
-           /* if(document.querySelector(searchClass).innerHTML == "")
-            {
-                document.querySelector(searchClass).innerHTML = "No results...";
-            }*/
-    
-    
-            function clearResults(){
-                document.querySelector(searchClass). innerHTML = "";
-            }
-    
-
-        ////////////////////////////////main////////////////////////////////////
-
-
-
-        for(let i =0; i < database.children.length; i++){
-            for(let y = 0; y < database.children[i].children.length; y++){
-                if(database.children[i].children[y].name == selected) 
-                {
-                    document.querySelector(searchClass).innerHTML += `
-                    
-                    <div class="col-xs-12 col-sm-9 col-md-6 col-lg-7">
-                    <main>
-                      <div class="products">
-                            <img id='${database.children[i].children[y].name}' class="selectedImage" src='../img/${database.children[i].children[y].path}' alt='${database.children[i].children[y].name}'>
-                      </div>
-                    </main>
-                </div>
-
-                    `;
-                    break;
-                }
-            }
-        }
-
-
-        })
-        .catch((error)=>{
-            console.error(error);
-        })
-    }
-
-///////////////////first render///////////
-
-//////////////////////////////////////////
-
-
-
-    printResults('.mainContainer')
-    let page = "landing";
-    let selectedImage= "";
-    document.querySelector('.searchBar').addEventListener("keyup",  function(){
-       if(page == "landing")
-       {
-        printResults('.mainContainer', page)
-       }
-       else
-       {
-        document.querySelector('.products').innerHTML = ``;
-        showSelected('.mainContainer', selectedImage, page);
-       }
-    
-    });
-    document.querySelector('.mainContainer').onclick = function(event) {
-        page = "second"
-        selectedImage = event.target.id;
-        document.querySelector('.products').innerHTML = ``;
-        showSelected('.mainContainer', event.target.id, page);
-    }
+//////////////////////////////////////////////////////////
---
+++ index3.js
@@ -0,0 +1,213 @@
+   function printResults(searchClass, page) {
+       console.log(document.querySelector('.searchBar').value);
+       fetch('img/desktop.json')
+    .then(res => res.json())
+    .then(data =>{
+        const database = data;
+        const search = document.querySelector('.searchBar').value;
+       // console.log(${database.children[i].children[y].path);
+        clearResults();
+        let counter = 0;
+        let buffer = "";
+        buffer = `
+              <div>
+                <main>
+                <div class="products">
+        `;
+        for(let i =0; i < database.children.length; i++){
+
+            let temporaryBuffer= "";
+            let haveItems = false;
+            temporaryBuffer += `
+                
+            <div class="category"> 
+                <h3>${database.children[i].name}</h3>
+            </div>
+            
+            <div class="wrapper grid">
+            `; 
+
+            for(let y = 0; y < database.children[i].children.length; y++){
+                if(database.children[i].children[y].name.toLocaleLowerCase().includes(search) || search == "") 
+                {
+                    counter++;
+                    haveItems = true;
+                    temporaryBuffer += `
+                    <div class="item">
+                        <figure class="fig">
+                            <img id='${database.children[i].children[y].name}' type="button" src='../img/${database.children[i].children[y].path}' alt='${database.children[i].children[y].name}'>
+                            <figcaption>${database.children[i].children[y].name}</figcaption>
+                        </figure>
+                    </div>
+                    `; 
+                }
+            }
+            temporaryBuffer += `</div>
+            `;   
+            if(haveItems)
+            {
+ 
+                buffer += temporaryBuffer;
+
+            }
+        }
+        buffer +=`
+          </div>
+        </main>
+    </div>
+        `;
+        document.querySelector(searchClass).innerHTML = buffer;
+        document.querySelector('.counter').innerHTML = `${counter} layouts found`;
+
+
+       /* if(document.querySelector(searchClass).innerHTML == "")
+        {
+            document.querySelector(searchClass).innerHTML = "No results...";
+        }*/
+
+
+        function clearResults(){
+            document.querySelector(searchClass). innerHTML = "";
+        }
+
+    })
+    .catch((error)=>{
+        console.error(error);
+    })
+}
+////////////////first render/////////////////
+////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+    function showSelected(searchClass, selected, page)
+    {
+        fetch('img/desktop.json')
+        .then(res => res.json())
+        .then(data =>{
+            const database = data;
+            const search = document.querySelector('.searchBar').value;
+            clearResults();
+            let counter = 0;
+            let buffer = "";
+            buffer = `
+                <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+                    <nav>
+                        <div class="navigationProducts">
+            `;
+            for(let i =0; i < database.children.length; i++){
+    
+                let temporaryBuffer= "";
+                let haveItems = false;
+                temporaryBuffer += `
+                    
+                <div class="category"> 
+                    <h3>${database.children[i].name}</h3>
+                </div>
+                
+                <div>
+                `; 
+    
+                for(let y = 0; y < database.children[i].children.length; y++){
+                    if(database.children[i].children[y].name.toLocaleLowerCase().includes(search) || search == "") 
+                    {
+                        counter++;
+                        haveItems = true;
+                        temporaryBuffer += `
+                        <div class="item">
+                            <figure class="fig">
+                                <img id='${database.children[i].children[y].name}' class="img" type="button" src='../img/${database.children[i].children[y].path}' alt='${database.children[i].children[y].name}'>
+                                <figcaption>${database.children[i].children[y].name}</figcaption>
+                            </figure>
+                        </div>
+                        `; 
+                    }
+                }
+                temporaryBuffer += `</div>
+                `;   
+                if(haveItems)
+                {
+     
+                    buffer += temporaryBuffer;
+    
+                }
+            }
+            buffer +=`
+            </div>
+            </nav>
+          </div>
+
+            `;
+            document.querySelector(searchClass).innerHTML = buffer;
+            document.querySelector('.counter').innerHTML = `${counter} layouts found`;
+    
+    
+           /* if(document.querySelector(searchClass).innerHTML == "")
+            {
+                document.querySelector(searchClass).innerHTML = "No results...";
+            }*/
+    
+    
+            function clearResults(){
+                document.querySelector(searchClass). innerHTML = "";
+            }
+    
+
+        ////////////////////////////////main////////////////////////////////////
+
+
+
+        for(let i =0; i < database.children.length; i++){
+            for(let y = 0; y < database.children[i].children.length; y++){
+                if(database.children[i].children[y].name == selected) 
+                {
+                    document.querySelector(searchClass).innerHTML += `
+                    
+                    <div class="col-xs-12 col-sm-9 col-md-6 col-lg-7">
+                    <main>
+                      <div class="products">
+                            <img id='${database.children[i].children[y].name}' class="selectedImage" src='../img/${database.children[i].children[y].path}' alt='${database.children[i].children[y].name}'>
+                      </div>
+                    </main>
+                </div>
+
+                    `;
+                    break;
+                }
+            }
+        }
+
+
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+    }
+
+///////////////////first render///////////
+
+//////////////////////////////////////////
+
+
+
+    printResults('.mainContainer')
+    let page = "landing";
+    let selectedImage= "";
+    document.querySelector('.searchBar').addEventListener("keyup",  function(){
+       if(page == "landing")
+       {
+        printResults('.mainContainer', page)
+       }
+       else
+       {
+        document.querySelector('.products').innerHTML = ``;
+        showSelected('.mainContainer', selectedImage, page);
+       }
+    
+    });
+    document.querySelector('.mainContainer').onclick = function(event) {
+        page = "second"
+        selectedImage = event.target.id;
+        document.querySelector('.products').innerHTML = ``;
+        showSelected('.mainContainer', event.target.id, page);
+    }
